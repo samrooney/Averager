3ba66429ffb38c8fc55e981a846c44b2e93d54cd
samrooney <sam_rooney@asl.org>
2014-10-23 14:40:25 +0100

    Sam Rooney averager assignment

--- Averager.java
+++ Averager.java
@@ -2,15 +2,43 @@
 *
 * Reads in scores until a negative value is encountered, at which point it determines and prints the average
 *
-* @author <your name here>
-* @version <date you finished the code>
+* @author <Sam Rooney>
+* @version <17/10/14>
 *
 */
 
+ import java.util.Scanner;
 public class Averager {
-	
-	public static void main(String[] args) {
-		// TODO - Put your code here
-	}
-
-}
+   public static void main(String[] args) {
+      
+      Scanner s = new Scanner(System.in);
+      
+      double numTest = 0.0;
+      
+      double totalTest = 0.0;
+      
+      System.out.println("Enter number you would like to be averged, ");
+      
+      System.out.println ("when finished enter value -7.");
+      
+      double addTest = s.nextDouble();
+      
+      while (addTest != -7) { // while addTest does NOT have the value of -5
+      
+      totalTest = totalTest + addTest;
+      
+      numTest = numTest + 1;
+      
+      System.out.println("Do me a favour and enter another number");
+      
+      System.out.println("or -7 to exit will ya.");
+     
+      addTest = s.nextDouble();
+      }
+      System.out.println("The average of numbers is: " + totalTest / numTest);
+   }
+}      
+      
+      
+      
+      
